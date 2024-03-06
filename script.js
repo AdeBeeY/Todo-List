7763c9b92ec6123cbee695038fdfc6be67b7572e
Adebayo Adewusi <adewusiadebayo@gmail.com>
2024-03-06 19:54:18 +0100

    change todoList into an object, add date and button elements

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-const btn = document.querySelector('.js-btn');
+const addBtn = document.querySelector('.js-btn');
 const input = document.querySelector('.js-input');
 const output = document.querySelector('.js-output');
+const dueDate = document.querySelector('.js-due-date');
 
 let todoList = [
-  'Buy Gas',
- 'Go to the Cinema', 
- 'Medical Check-up'
+  {
+    name: 'Buy Gas',
+    date: '10-02-2024'
+  },
+  {
+    name: 'Go to the Cinema',
+    date: '10-07-2024'
+  },
+  {
+    name: 'Medical Check-up',
+    date: '10-07-2024'
+  }
 ];
 
 function displayTodoList() {
   let todoTotal = '';
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    let name = todoList[i].name;
+    let date = todoList[i].date;
+    console.log(name);
+    console.log(date);
+    const html = `
+        <div>
+          ${name}
+        </div>
+        <div>
+          ${date}
+        </div>
+        <button onclick="
+          todoList.splice(${i}, 1);
+          displayTodoList();
+        " >
+          Delete
+        </button>
+    `;
     todoTotal += html;
   }
   output.innerHTML = todoTotal;
 }
-
 displayTodoList();
 
-btn.addEventListener('click', () => {
-  const item = input.value;
-  todoList.push(item);
+addBtn.addEventListener('click', () => {
+  const name = input.value;
+  const date = dueDate.value;
+
+  todoList.push({
+    name: name,
+    date: date
+  });
 
   input.value = '';
+  dueDate.value = '';
 
   displayTodoList();
 });
